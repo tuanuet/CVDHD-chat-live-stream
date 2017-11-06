fb02526ecf7ce34ff1ca225b860f0554e5483537
tuan.vm <tuan.uet@gmail.com>
2017-11-06 16:14:59 +0700

    abc

--- dist/app.js
+++ dist/app.js
@@ -114,7 +114,7 @@ app.use(function (req, res, next) {
   }
   next();
 });
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //import router
 
