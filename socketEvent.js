1bcd9e3586d3e2778a2b5549eb65ab7cee17fcfd
tuan.vm <tuan.uet@gmail.com>
2017-11-25 18:23:54 +0700

    encode to mp4 file < x2.5

--- socketEvent.js
+++ socketEvent.js
@@ -1,5 +1,7 @@
 var ss = require('socket.io-stream');
-
+import fs from 'fs';
+import path from 'path';
+var ffmpeg = require('fluent-ffmpeg');
 module.exports = (io) => {
   io.of('/livestream').on('connection',(socket) => {
     console.log('Event connection: ',socket.id);
@@ -7,17 +9,76 @@ module.exports = (io) => {
     socket.on('join',(data,cb) => {
       socket.join(data.roomId,(err) => {
         if(err) return cb(false);
+        try{
+            let pathDirectory = `${__dirname}/uploads/${data.roomId}/`
+            let pathEncode = `${__dirname}/encodes/${data.roomId}/`
+            let existFolder = fs.existsSync(pathDirectory);
+            let existFolderEncode = fs.existsSync(pathEncode);
+            if(!existFolder) {
+                fs.mkdirSync(pathDirectory);
+            }
 
-        socket.roomId = data.roomId;
-        return cb(true);
+            if(!existFolderEncode) {
+                fs.mkdirSync(pathEncode);
+            }
+
+            let countSegment = fs.readdirSync(pathDirectory).length;
+            socket.buffers = [];
+            socket.roomId = data.roomId;
+            socket.countSegment = countSegment;
+            return cb(true);
+        } catch (err) {
+            return cb(false);
+        }
+
       })
-
     })
-    socket.on('streaming', function(blob) {
-      console.log('Event streaming: ',blob);
 
-      socket.broadcast.to(socket.roomId).emit('server-broadcast-livestream',blob)
+    socket.on('streaming', async (blob) => {
+      try{
+        if(!socket.roomId && !socket.countSegment) throw new Error('socket not join room')
+        let countSegment = ++socket.countSegment;
+        let pathSegment = path.join(__dirname,`/uploads/${socket.roomId}/blob-${countSegment}.webm`);
+        let pathSend = path.join(__dirname,`/encodes/${socket.roomId}/blob-${countSegment}.mp4`)
 
+        socket.buffers.push(blob);
+        await writeFileMp4(pathSegment,blob);
+        // TODO: emit data ??
+
+        ffmpeg(pathSegment)
+          .videoCodec('libx264')
+          .audioCodec('libmp3lame')
+          .size('320x240')
+          .on('error', function(err) {
+            console.log('An error occurred: ' + err.message);
+          })
+          .on('end', function() {
+            console.log('Processing finished !');
+            let data = fs.readFileSync(pathSend);
+            socket.broadcast.to(socket.roomId).emit('server-broadcast-livestream',data)
+          })
+          .save(pathSend);
+
+
+        //=================
+
+      } catch ( err ) {
+        console.log(err.message);
+      }
+    });
+
+    let writeFileMp4 = (path,buffer) => new Promise((resolve,reject) => {
+      // open the file in writing mode, adding a callback function where we do the actual writing
+      fs.open(path, 'w', function(err, fd) {
+          if (err) reject(err)
+          // write the contents of the buffer, from position 0 to the end, to the file descriptor returned in opening our file
+          fs.write(fd, buffer, 0, buffer.length, null, function(err) {
+              if (err) throw 'error writing file: ' + err;
+              fs.close(fd, function() {
+                  resolve(true);
+              });
+          });
+      });
     });
   });
 
